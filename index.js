bc1e0c5ae7ec1c7e1dda5dfc54ac2e2278dce706
Sam Roble <smroble1996@gmail.com>
2021-07-26 19:22:14 -0600

    commit tree 04f18f43889341237503c4b68da4b9e6f8d43be8

---
+++ index.js
@@ -0,0 +1,15 @@
+divide = () => {return 2000/100};
+
+// let square = () =>{}
+let x = 2;
+function square(x){
+    return (x**2);
+};
+
+// add = () =>{}
+
+let y = 5;
+function add (x, y){
+ return x+y;
+}
+
